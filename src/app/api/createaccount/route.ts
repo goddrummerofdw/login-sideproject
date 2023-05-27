fca531ca99a4bfe59993ebf19fd6cce890c431b5
Solrac <solrac@Carloss-MacBook-Pro.local>
2023-05-26 21:20:21 -0400

    added password encryption

--- src/app/api/createaccount/route.ts
+++ src/app/api/createaccount/route.ts
@@ -1,22 +1,35 @@
 import throwError from "@/app/throwerror"
 import { NextResponse } from "next/server"
 import User from '../../mongooseschema'
+import bcrypt from 'bcrypt';
 
 export async function POST(request: Request) {
     try {
         const { email, firstName, lastName, password } = await request.json()
-        // const lowerCaseEmail = email.toLowerCase().split(' ').map((char: string) => char.trim()).join('');
         let fields = [firstName, lastName, email]
         const mapedFields = fields.map((field) => {
             return field.toLowerCase().split(' ').map((char: string) => char.trim()).join('');
         })
 
+        const hashPassword = (password: string, saltRounds: number) => {
+            return new Promise((resolve, reject) => {
+                bcrypt.hash(password, saltRounds, (err, hash) => {
+                    if (err) {
+                        console.error('Error hashing password:', err);
+                        reject(err);
+                    } else {
+                        resolve(hash);
+                    }
+                });
+            });
+        }
         let user = await User.findOne({ email: mapedFields[2] })
+        const hashedPassword = await hashPassword(password, 10)
         if (user) {
             console.log(`Found User ${user}`)
             return NextResponse.json({ status: 404, message: "User Already Exists , Please Try Logging in" })
         } else {
-            user = new User({ firstName: mapedFields[0], lastName: mapedFields[1], email: mapedFields[2], password: password });
+            user = new User({ firstName: mapedFields[0], lastName: mapedFields[1], email: mapedFields[2], password: hashedPassword });
             const savedUser = await user.save();
             console.log('User created:', savedUser);
         }
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,16 +1,21 @@
 import throwError from "@/app/throwerror"
 import { NextResponse } from "next/server"
-import { NextRequest } from "next/server"
 import User from '../../mongooseschema'
+import bcrypt from 'bcrypt'
 
 export async function POST(request: Request) {
     try {
         const { email, password } = await request.json()
-        console.log(NextRequest)
-        let user = await User.findOne({ email: email, password: password })
-        if (user) {
-            console.log(`Found User ${user}`)
-            return NextResponse.json({ status: 200, message: "Dashboard Access Granted" })
+        const userObject: { [key: string]: any } | null = await User.findOne({ email: email })
+        if (userObject) {
+            console.log(`Found User ${userObject}`)
+            const match = await bcrypt.compare(password, userObject.password);
+            if (match) {
+                return NextResponse.json({ status: 200, message: "Dashboard Access Granted" })
+            } else {
+                return NextResponse.json({ status: 401, message: "Invalid Credentials" })
+            }
+
         } else {
             return NextResponse.json({ status: 401, message: "Invalid Credentials" })
         }
