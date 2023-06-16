648cbf1fffae682a963fe4b26578fbcf66e6aeef
Solrac <solrac@Carloss-MacBook-Pro.local>
2023-06-16 16:25:05 -0400

    fixing deployment

--- middleware.ts
+++ middleware.ts
@@ -2,16 +2,20 @@ import { NextRequest, NextResponse } from 'next/server';
 import { verifyAuth } from '@/app/lib/auth';
 
 export async function middleware(req: NextRequest) {
-    // console.log("middleware being triggered", req.nextUrl.pathname)
+    console.log("middleware being triggered", req.nextUrl.pathname)
     const token = req.cookies.get('user-token')?.value
+
     const verifiedToken = token && await verifyAuth(token).catch((err) => console.log(err))
+    console.log(verifiedToken, 'This is the verified token')
     if (req.nextUrl.pathname.startsWith('/login') && !verifiedToken) {
+        console.log('This checks for no token')
         return;
     }
     if (req.nextUrl.pathname.startsWith('/login') && verifiedToken) {
         return NextResponse.redirect(new URL('/dashboard', req.url));
     }
-    if (!verifiedToken) {
+    if (req.nextUrl.pathname.startsWith('/') && !verifiedToken) {
+        console.log('No token')
         return NextResponse.redirect(new URL('/login', req.url));
     }
     if (req.nextUrl.pathname.startsWith('/createaccount') && verifiedToken) {
